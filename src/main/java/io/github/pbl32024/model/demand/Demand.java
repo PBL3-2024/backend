ab9cabbce954591a8a35d252bcef1b0d6141d335
Jesse Grabowski <github@jessegrabowski.com>
2024-06-29 11:00:59 +0900

    Integrate demand database tables

--- src/main/java/io/github/pbl32024/model/demand/Demand.java
+++ src/main/java/io/github/pbl32024/model/demand/Demand.java
@@ -1,12 +1,19 @@
 package io.github.pbl32024.model.demand;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table(schema = "public", name = "demand")
 public class Demand {
 
+	@Id
+	@Column("soc")
 	private String socCode;
 
+	@Column("value")
 	private long value;
 
 }
--- src/main/java/io/github/pbl32024/model/demand/DemandDAO.java
+++ src/main/java/io/github/pbl32024/model/demand/DemandDAO.java
@@ -1,7 +1,10 @@
 package io.github.pbl32024.model.demand;
 
 
+import io.github.pbl32024.SOCSupport;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -10,19 +13,32 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 @RequiredArgsConstructor
 public class DemandDAO {
-	private final Map<String,Demand> map= new ConcurrentHashMap<>(); //"we dont have a database so make a map"
+	private final NamedParameterJdbcTemplate jdbcTemplate;
 
 	public Demand getDemand(DemandQuery query) {
-		return map.get(query.getSocCode());
+		Long demand = jdbcTemplate.queryForObject("""
+				SELECT SUM(value) AS total_value
+				FROM demand
+				WHERE soc LIKE CONCAT(:soc, '%');
+				""", Map.of("soc", SOCSupport.trimSoc(query.getSocCode())), Long.class);
+
+		Demand result = new Demand();
+		result.setSocCode(query.getSocCode());
+		result.setValue(demand);
+		return result;
 	}
 
 	public Demand setDemand(Demand demand) {
-		map.put(demand.getSocCode(),demand);
+		MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+		parameterSource.addValue("soc", demand.getSocCode());
+		parameterSource.addValue("value", demand.getValue());
+		jdbcTemplate.update("""
+				INSERT INTO demand (soc, value)
+				VALUES (:soc, :value)
+				ON CONFLICT (soc) DO UPDATE
+				SET VALUE = :value;
+				""", parameterSource);
 		return demand;
-	}
-
-	public void save(Demand demand) {
-
 	}
 
 }
