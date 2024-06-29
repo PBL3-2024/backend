23a9c3d48b2808a3ab25bc51373baa2dfc6cb8cb
Jesse Grabowski <github@jessegrabowski.com>
2024-06-29 14:22:10 +0900

    Add analytics ingestion

--- src/main/java/io/github/pbl32024/model/analytics/AnalyticsDAO.java
+++ src/main/java/io/github/pbl32024/model/analytics/AnalyticsDAO.java
@@ -1,11 +1,20 @@
 package io.github.pbl32024.model.analytics;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class AnalyticsDAO {
+
+	private final NamedParameterJdbcTemplate jdbcTemplate;
 
 	public Chart getUserCurrentOccupationPieChart(AnalyticsQuery query) {
 		return null;
@@ -79,8 +88,24 @@ public class AnalyticsDAO {
 		return 0;
 	}
 
-	public void saveClickData(ClickData query) {
-
+	public void saveClickData(List<ClickData> data) {
+		SqlParameterSource[] params = data.stream()
+				.map(d -> {
+					MapSqlParameterSource param = new MapSqlParameterSource();
+					param.addValue("timestamp", Instant.ofEpochMilli(d.getTimestamp()).atOffset(ZoneOffset.UTC));
+					param.addValue("elementId", d.getElementId());
+					param.addValue("elementType", d.getElementType().name());
+					param.addValue("userId", d.getUserId());
+					param.addValue("userPostalCode", d.getUserPostalCode());
+					param.addValue("userCurrentSoc", d.getUserCurrentOccupation());
+					param.addValue("userGoalSoc", d.getUserGoalOccupation());
+					return param;
+				})
+				.toArray(SqlParameterSource[]::new);
+		jdbcTemplate.batchUpdate("""
+				INSERT INTO click_data (timestamp, element_id, element_type, user_id, user_postal_code, user_current_soc, user_goal_soc)
+				VALUES (:timestamp, :elementId, :elementType, :userId, :userPostalCode, :userCurrentSoc, :userGoalSoc); 
+				""", params);
 	}
 
 }
