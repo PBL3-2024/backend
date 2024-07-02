3cd0510c551e4ce757f191e1852db09471d5b834
Hikaru Tokuyama <>
2024-07-02 14:01:02 +0900

    Forecast employment

--- src/main/java/io/github/pbl32024/model/employment/EmploymentController.java
+++ src/main/java/io/github/pbl32024/model/employment/EmploymentController.java
@@ -21,28 +21,8 @@ public class EmploymentController {
 
 	@GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	public EmploymentResponse getEmploymentBySocCode(String socCode) {
-		List<Employment> employments = new ArrayList<>();
-		Random random = new Random();
-		boolean uptrend = random.nextBoolean();
-		long employment = Math.abs(random.nextInt());
-		for (int i = 0; i < 10; i++) {
-			if (uptrend) {
-				employment = (long) (employment * random.nextDouble(0.95, 1.3));
-			} else {
-				employment = (long) (employment * random.nextDouble(0.6, 1.1));
-			}
-
-			Employment e = new Employment();
-			e.setSocCode(socCode);
-			e.setValue(employment);
-			e.setId(UUID.randomUUID().toString());
-			e.setDate(LocalDate.of(2018 + i, 1, 1).atStartOfDay());
-			e.setForecasted(2018 + i > 2024);
-			employments.add(e);
-		}
-
 		EmploymentResponse response = new EmploymentResponse();
-		response.setEmployment(employments);
+		response.setEmployment(employmentService.getEmploymentBySocCode(socCode));
 		return response;
 	}
 
--- src/main/java/io/github/pbl32024/model/employment/ForecastingService.java
+++ src/main/java/io/github/pbl32024/model/employment/ForecastingService.java
@@ -1,9 +1,13 @@
 package io.github.pbl32024.model.employment;
 
+import com.workday.insights.timeseries.arima.Arima;
+import com.workday.insights.timeseries.arima.struct.ArimaParams;
+import com.workday.insights.timeseries.arima.struct.ForecastResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,44 @@ public class ForecastingService {
 	private final ForecastingProperties forecastingProperties;
 
 	public List<Employment> forecast(List<Employment> employment) {
-		return null;
+		List<Employment> forecasted = new ArrayList<>(employment);
+		forecasted.sort(Comparator.comparing(Employment::getDate));
+
+		double[] data = forecasted.stream()
+				.mapToDouble(Employment::getValue)
+				.toArray();
+
+		int p = 3;
+		int d = 0;
+		int q = 3;
+		int P = 1;
+		int D = 1;
+		int Q = 0;
+		int m = 0;
+		int forecastSize = 3;
+
+		ArimaParams params = new ArimaParams(p, d, q, P, D, Q, m);
+
+// Obtain forecast result. The structure contains forecasted values and performance metric etc.
+		ForecastResult forecastResult = Arima.forecast_arima(data, forecastSize, params);
+
+// Read forecast values
+		double[] forecastData = forecastResult.getForecast();
+
+		int year = employment.getLast().getDate().getYear() + 1;
+		for (double point : forecastData) {
+			Employment forecastedEmployment = new Employment();
+			// fill in fields
+			forecastedEmployment.setDate(LocalDate.of(year, 5, 1).atStartOfDay());
+			forecastedEmployment.setValue((long) point);
+			forecastedEmployment.setSocCode(employment.get(0).getSocCode());
+			forecastedEmployment.setId(UUID.randomUUID().toString());
+			forecastedEmployment.setForecasted(true);
+			forecasted.add(forecastedEmployment);
+			year++;
+		}
+
+		return forecasted;
 	}
 
 }
