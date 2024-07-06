b9467103cd2f3fa2fca0cadd95841f35108073c2
Jesse Grabowski <github@jessegrabowski.com>
2024-07-06 17:41:09 +0900

    Add profile functionality

--- src/main/java/io/github/pbl32024/model/user/UserService.java
+++ src/main/java/io/github/pbl32024/model/user/UserService.java
@@ -2,22 +2,24 @@ package io.github.pbl32024.model.user;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-	private final Auth0Client auth0Client;
-
 	private final ProfileDAO profileDAO;
 
 	public Profile updateUserProfile(Profile userProfile) {
-		return null;
+		userProfile.setNew(getUserProfile(userProfile.getId()).isEmpty());
+		return profileDAO.save(userProfile);
 	}
 
-	public Profile getUserProfile(String id) {
-		return null;
+	public Optional<Profile> getUserProfile(String id) {
+		return profileDAO.getProfileById(id);
 	}
 
 	public void deleteUserProfile(String id) {
