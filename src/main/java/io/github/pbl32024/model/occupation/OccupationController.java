2c08241bfbd8e8ff981160f43c8ce98776ebbf28
Jesse Grabowski <github@jessegrabowski.com>
2024-07-06 10:26:31 +0900

    Configure security and caching

--- src/main/java/io/github/pbl32024/model/occupation/OccupationController.java
+++ src/main/java/io/github/pbl32024/model/occupation/OccupationController.java
@@ -2,10 +2,14 @@ package io.github.pbl32024.model.occupation;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,15 +19,20 @@ public class OccupationController {
 	private final OccupationService occupationService;
 
 	@GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Occupation getOccupation(String socCode) {
-		return occupationService.getOccupation(socCode);
+	public ResponseEntity<Occupation> getOccupation(String socCode) {
+		Occupation occupation = occupationService.getOccupation(socCode);
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(Duration.ofDays(1)))
+				.body(occupation);
 	}
 
 	@GetMapping(path = "/children/", produces = MediaType.APPLICATION_JSON_VALUE)
-	public OccupationResponse getChildOccupations(String socCode) {
+	public ResponseEntity<OccupationResponse> getChildOccupations(String socCode) {
 		OccupationResponse occupationResponse = new OccupationResponse();
 		occupationResponse.setOccupation(occupationService.getChildOccupations(socCode));
-		return occupationResponse;
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(Duration.ofDays(1)))
+				.body(occupationResponse);
 	}
 
 }
--- src/main/java/io/github/pbl32024/model/unemployment/UnemploymentController.java
+++ src/main/java/io/github/pbl32024/model/unemployment/UnemploymentController.java
@@ -2,10 +2,14 @@ package io.github.pbl32024.model.unemployment;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +19,12 @@ public class UnemploymentController {
 	private final UnemploymentService unemploymentService;
 
 	@GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-	public UnemploymentResponse getUnemploymentBySocCode(String socCode) {
+	public ResponseEntity<UnemploymentResponse> getUnemploymentBySocCode(String socCode) {
 		UnemploymentResponse unemploymentResponse = new UnemploymentResponse();
 		unemploymentResponse.setUnemployment(unemploymentService.getUnemploymentBySocCode(socCode));
-		return unemploymentResponse;
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(Duration.ofDays(1)))
+				.body(unemploymentResponse);
 	}
 
 }
